1c77cac20814579d42b1105a703b4342bfe3a79f
Mich <michxcrz@gmail.com>
2024-04-26 14:11:15 -0300

    testando webscraping no mercado livre

--- webscraping_pup/teste2/script.js
+++ webscraping_pup/teste2/script.js
@@ -10,5 +10,13 @@ const searchFor = "notebook";
     console.log("Estou funcionando");
     await page.goto(url);
     console.log("entrei na url");
+    await page.waitForSelector('#cb1-edit');
+    await page.type("#cb1-edit", searchFor);
+    await Promise.all([page.waitForNavigation(), page.click(".nav-search-btn")]);
+
+    const links = await page.$$eval(".ui-search-result__image > a", (e1) =>
+    e1.map((link) => link.href)
+);
+    console.log(links);
     await browser.close();
 })();

a60c9a9d2d8883e2429c6cf9f764bd5299e7aa09
Mich <125368412+michellycruz@users.noreply.github.com>
2024-05-13 01:45:54 -0300

    ?

---
+++ tailwindCSS/tailwindcss-ui/src/components/Header/Logo.jsx
@@ -0,0 +1,13 @@
+export default function Logo(){
+    return(
+        <span
+            className="
+                text-2xl font-bold uppercase
+                text-zinc-900 dark:text-zinc
+            "
+        >
+        </span>
+    )
+}
+
+//parei no min 7:31 da aula 5
--- tailwindCSS/tailwindcss-ui/src/components/Header/index.jsx
+++ tailwindCSS/tailwindcss-ui/src/components/Header/index.jsx
@@ -1,6 +1,16 @@
+import Logo from "./Logo"
+
 export default function Header(){
     return(
-        <header>
+        <header
+            className="
+                px-4 h-16
+                flex items-center justify-between
+                gap-6
+                bg-zinc-100 dark:bg-zinc-800
+            "
+        >
+            <Logo />
             <div>logo</div>
             <div>pesquisa</div>
             <div>avatar</div>

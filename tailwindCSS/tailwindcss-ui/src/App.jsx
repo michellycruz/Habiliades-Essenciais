08db18a5d1b692ff6f097b08fe12228655a96372
Mich <125368412+michellycruz@users.noreply.github.com>
2024-05-13 00:18:42 -0300

    ?

--- tailwindCSS/tailwindcss-ui/src/App.jsx
+++ tailwindCSS/tailwindcss-ui/src/App.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="h-screen bg-zinc-100 text dark:bg-zinc-800 dark:text-zinc-50 grid place-content-center">
-      <h1 className='text-3xl text-center'>Ready to start with Taildwind!</h1>
-      <button 
-        className='
+    <>
+      <Header />
+      <div className="h-screen bg-zinc-100 text dark:bg-zinc-800 dark:text-zinc-50 grid place-content-center">
+        <h1 className='text-3xl text-center'>Ready to start with Taildwind!</h1>
+        <button 
+          className='
           block w-fit mx-auto my-8 px-4 py-2 rounded-lg
           border-transparent hover:border-cyan-500 border-2
           transition-colors
-        '
-        onClick={() => setCount(count => count + 1)}>
-        count is {count}
-      </button>
-    </div>
+          '
+          onClick={() => setCount(count => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
   )
 }
 

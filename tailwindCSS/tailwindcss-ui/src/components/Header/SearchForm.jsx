398e837569afbb906b75dec160be6e85cafff5f6
Mich <michxcrz@gmail.com>
2024-05-14 17:46:41 -0300

    Update SearchForm.jsx

--- tailwindCSS/tailwindcss-ui/src/components/Header/SearchForm.jsx
+++ tailwindCSS/tailwindcss-ui/src/components/Header/SearchForm.jsx
@@ -11,12 +11,19 @@ export default function SearchForm(){
                 className="
                     min-w-0 w-full p-2 rounded-l-lg border-2 border-transparent
                     bg-zinc-200 dark:bg-zinc-700 text-zinc-900 dark:text-zinc-50
-                    focus:outline-none
+                    focus:outline-none focus:border-emerald-400 transition-colors
                 "
                 type="text"
                 placeholder="Pesquisar..."
             />
-            <button>
+            <button
+                className="
+                    min-w-0 py-2 px-4 rounded-r-lg border-2 border-transparent
+                    bg-zinc-200 dark:bg-zinc-700 text-zinc-900 dark:text-zinc-50
+                    focus:outline-none focus:border-emerald-400 focus:text-emerald-400 hover:border-emerald-400 hover:text-emerald-400 transition-colors
+                "
+            
+            >
                 <FiSearch />
             </button>
         </form>

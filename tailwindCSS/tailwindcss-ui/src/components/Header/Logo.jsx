69400f1b8510707016e4f5eb9af786917d877580
Mich <michxcrz@gmail.com>
2024-05-14 17:52:42 -0300

    responsividade

--- tailwindCSS/tailwindcss-ui/src/components/Header/Logo.jsx
+++ tailwindCSS/tailwindcss-ui/src/components/Header/Logo.jsx
@@ -2,7 +2,7 @@ export default function Logo(){
     return(
         <span
             className="
-                text-2xl font-bold uppercase
+                text-2xl xl:text-3xl font-bold uppercase
                 text-zinc-900 dark:text-zinc-50
             "
         >
--- tailwindCSS/tailwindcss-ui/src/components/Header/index.jsx
+++ tailwindCSS/tailwindcss-ui/src/components/Header/index.jsx
@@ -6,9 +6,9 @@ export default function Header(){
     return(
         <header
             className="
-                px-4 h-16
+                px-4 xl:px-6 h-16 lg:h-20
                 flex items-center justify-between
-                gap-6
+                gap-6 md:gap-16 xl:gap-32
                 bg-zinc-100 dark:bg-zinc-800
             "
         >

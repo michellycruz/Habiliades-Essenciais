bb06ee9121fdee10740dad7161d4ecbfe9fb5128
Mich <michxcrz@gmail.com>
2024-05-15 18:25:49 -0300

    icones menu

--- tailwindCSS/tailwindcss-ui/src/components/Sidebar/Button.jsx
+++ tailwindCSS/tailwindcss-ui/src/components/Sidebar/Button.jsx
@@ -1,8 +1,13 @@
 // eslint-disable-next-line react/prop-types
-export default function Button({ children }){
+export default function Button({ active, children }){
+    const backgroundColor = active ? "bg-zinc-200 dark:bg-zinc-900" : ""
     return (
         <button
-            className=""
+            className= {`
+                 py-2 px-4 w-full text-left rounded-lg 
+                 ${backgroundColor} transition-colors
+                 hover:bg-zinc-200 dark:hover:bg-zinc-900
+                `}
         >
             {children}
         </button>
--- tailwindCSS/tailwindcss-ui/src/components/Sidebar/index.jsx
+++ tailwindCSS/tailwindcss-ui/src/components/Sidebar/index.jsx
@@ -1,19 +1,32 @@
 import WriteButton from "./WriteButton";
-import { FiEdit3 } from "react-icons/fi";
+import { FiEdit3, FiSend, FiInbox, FiFileText, FiTrash2, FiAlertOctagon, FiArchive } from "react-icons/fi";
 import Button from "./Button";
+const inlineIcon = "inline mr-1 mb-1"
 
 export default function Sidebar(){
     return(
-        <aside className="flex flex-col">
+        <aside className="hidden md:flex flex-col items-start gap-2 grow-0 shrink basis-48">
             <WriteButton>
-                <FiEdit3 className="inline mr-1 mb-1" />Escrever
+                <FiEdit3 className={inlineIcon} />Escrever
             </WriteButton>
-            <Button>Caixa de Entrada</Button>
-            <Button>Enviados</Button>
-            <Button>Rascunhos</Button>
-            <Button>Arquivados</Button>
-            <Button>Spam</Button>
-            <Button>Lixeira</Button>
+            <Button active>
+                <FiInbox className={inlineIcon}/>    Caixa de Entrada
+            </Button>
+            <Button>
+               <FiSend className={inlineIcon} /> Enviados
+            </Button>
+            <Button>
+               <FiFileText className={inlineIcon} /> Rascunhos
+            </Button>
+            <Button>
+               <FiArchive className={inlineIcon} /> Arquivados
+            </Button>
+            <Button>
+               <FiAlertOctagon className={inlineIcon} /> Spam
+            </Button>
+            <Button>
+                <FiTrash2  className={inlineIcon} /> Lixeira
+            </Button>
         </aside>
     )
 }

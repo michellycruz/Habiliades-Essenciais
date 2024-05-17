9f38b20ee4a9df978ab7b525d33019f03886ce00
Mich <michxcrz@gmail.com>
2024-05-17 17:31:26 -0300

    Update index.jsx

--- tailwindCSS/tailwindcss-ui/src/components/Bottombar/index.jsx
+++ tailwindCSS/tailwindcss-ui/src/components/Bottombar/index.jsx
@@ -1,18 +1,44 @@
-import { FiEdit3 } from "react-icons/fi";
+import { useState } from "react";
 import Button from "./Button";
+import { FiEdit3, FiFileText, FiInbox, FiMenu, FiX } from "react-icons/fi";
 
 const inlineIcon = "inline mb-1 sm:mr-2"
-const smOnlyText = "hidden "
+const smOnlyText = "hidden sm:inline"
 
-export default function Bottombar() {
-    return (
-        <div>
-            <Button>
-                <FiEdit3 className={inlineIcon} />
-                <span className={smOnlyText}>Escrever</span>
-            </Button>
-        </div>
-    )
+const Bottombar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  return (
+    <div className="w-full flex gap-2 md:hidden items-center relative">
+      <Button>
+        <FiEdit3 className={inlineIcon} />
+        <span className={smOnlyText}>Escrever</span>
+      </Button>
+      <Button active>
+        <FiInbox className={inlineIcon} />
+        <span className={smOnlyText}>Caixa de Entrada</span>
+      </Button>
+      <Button>
+        <FiFileText className={inlineIcon} />
+        <span className={smOnlyText}>Rascunhos</span>
+      </Button>
+      <Button onClick={() => setIsMenuOpen(state => !state)}>
+        {isMenuOpen ? (
+          <>
+            <FiX className={inlineIcon} />
+            <span className={smOnlyText}>Fechar</span>
+          </>
+        ) : (
+          <>
+            <FiMenu className={inlineIcon} />
+            <span className={smOnlyText}>Mais</span>
+          </>
+        )}
+      </Button>
+    </div>
+  )
 }
+
+export default Bottombar
 
 //parei no minuto 4:29 da aula 7
